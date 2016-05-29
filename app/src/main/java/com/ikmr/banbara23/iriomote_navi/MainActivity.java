eb488807eb733258c3cd1128ea8a89278d5d00ae
池村 和剛 <kazutakai@tt-com.net>
2016-05-30 07:19:52 +0900

    first commit

---
+++ app/src/main/java/com/ikmr/banbara23/iriomote_navi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ikmr.banbara23.iriomote_navi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
