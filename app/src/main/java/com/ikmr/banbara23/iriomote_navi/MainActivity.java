15be54711df2ad3b8d7ce9529f3eb674d3958c7d
池村 和剛 <kazutakai@tt-com.net>
2016-06-11 18:08:55 +0900

    トップ画面に３タブ表示まで実装

--- app/src/main/java/com/ikmr/banbara23/iriomote_navi/MainActivity.java
+++ app/src/main/java/com/ikmr/banbara23/iriomote_navi/MainActivity.java
@@ -1,7 +1,10 @@
+
 package com.ikmr.banbara23.iriomote_navi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void foodClick(View view) {
+        Intent intent = new Intent(this, ItemListActivity.class);
+        startActivity(intent);
+    }
+
+    public void hotelClick(View view) {
+        Intent intent = new Intent(this, ItemListActivity.class);
+        startActivity(intent);
+    }
+
+    public void photoClick(View view) {
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
     }
 }
