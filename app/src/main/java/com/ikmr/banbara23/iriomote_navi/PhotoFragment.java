5dcd76172dd841cc141c4422d9c155be79a21275
池村 和剛 <kazutakai@tt-com.net>
2016-06-12 23:04:19 +0900

    ScrollFragment内にコンテンツを仮に作って置いてみた

---
+++ app/src/main/java/com/ikmr/banbara23/iriomote_navi/PhotoFragment.java
@@ -0,0 +1,24 @@
+
+package com.ikmr.banbara23.iriomote_navi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class PhotoFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        listView = (AsymmetricGridView) findViewById(R.id.listView);
+//
+//        // Choose your own preferred column width
+//        listView.setRequestedColumnWidth(Utils.dpToPx(this, 120));
+//        final List<AsymmetricItem> items = new ArrayList<>();
+//
+//        // initialize your items array
+//        adapter = new ListAdapter(this, listView, items);
+//        AsymmetricGridViewAdapter asymmetricAdapter =
+//                new AsymmetricGridViewAdapter<>(this, listView, adapter);
+//        listView.setAdapter(asymmetricAdapter);
+    }
+}
